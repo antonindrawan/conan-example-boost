548576051ecc6da2def0f727146e0216e8fba1e7
Anton Indrawan <anton.indrawan@gmail.com>
2020-03-27 18:12:31 +0100

    fix: use the new way of python_requires
    Waiting for conan 1.24.0 for the init() method

--- conanfile.py
+++ conanfile.py
@@ -1,23 +1,40 @@
 from conans import ConanFile, CMake, python_requires
 
 
-base = python_requires("conan-shared/0.1@demo/testing")
+# Old way of python_requires
+#base = python_requires("conan-shared/0.1@demo/testing")
 
-class BoostExampleConan(base.Base):
+# Old way
+# class BoostExampleConan(base.Base)
+class BoostExampleConan(ConanFile):
     name = "BoostExample"
     version = "1.1"
     description = "Example of Conan Package - Boost"
     license = "MIT"
     settings = "os", "compiler", "build_type", "arch"
     requires = "boost/[>=1.71.0]@conan/stable" # comma-separated list of requirements
-    python_requires = "conan-shared/0.1@demo/testing"
-    generators = "cmake"
 
-    # How to append an option?
-    default_options = {"boost:shared": True, "werror": True}
+    # New way of python_requires
+    python_requires = "conan-shared/0.1@demo/testing"
+    python_requires_extend = "conan-shared.Base"
+
+    generators = "cmake"
 
     scm = {
         "type": "git",
         "url": "https://github.com/antonindrawan/conan-example-boost.git",
         "revision": "master"
     }
+
+    # The temporary solution to extend base class' options for the old way of python_requires:
+    #default_options = dict(base.Base.default_options, **{
+    #    "boost:shared": True
+    #})
+
+    # TODO: Use the new method init() to append default options (to be released in conan 1.24.0)
+    # (https://github.com/conan-io/conan/pull/6614)
+    default_options = {"boost:shared": True, "werror": False}
+
+    def build(self):
+        base = self.python_requires["conan-shared"].module.Base
+        base.build(self)
