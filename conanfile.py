28c527fcfeb08d40b9a95971f04d53a7dc89d4ed
Anton Indrawan <anton.indrawan@gmail.com>
2020-03-27 12:19:40 +0100

    Use the shared conanfile

--- conanfile.py
+++ conanfile.py
@@ -1,38 +1,23 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, python_requires
 
-class BoostExampleConan(ConanFile):
+
+base = python_requires("conan-shared/0.1@demo/testing")
+
+class BoostExampleConan(base.Base):
     name = "BoostExample"
     version = "1.0.1"
     description = "Example of Conan Package - Boost"
     license = "MIT"
     settings = "os", "compiler", "build_type", "arch"
     requires = "boost/[>=1.71.0]@conan/stable" # comma-separated list of requirements
+    python_requires = "conan-shared/0.1@demo/testing"
     generators = "cmake"
-    default_options = {"boost:shared": True}
+
+    # How to append an option?
+    default_options = {"boost:shared": True, "werror": True}
 
     scm = {
         "type": "git",
         "url": "https://github.com/antonindrawan/conan-example-boost.git",
         "revision": "master"
     }
-
-    def _configure_cmake(self):
-        cmake = CMake(self, generator='Ninja')
-
-        if self.develop:
-            self.output.info("[develop mode], enabling warnings-as-errors")
-            cmake.definitions["ENABLE_WERROR"] = "ON"
-        else:
-            self.output.info("[consumer mode], not enabling warnings-as-errors")
-            cmake.definitions["ENABLE_WERROR"] = "OFF"
-
-        cmake.configure()
-        return cmake
-
-    def build(self):
-        cmake = self._configure_cmake()
-        cmake.build()
-
-    def package(self):
-        cmake = self._configure_cmake()
-        cmake.install()
