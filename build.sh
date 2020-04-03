870f20dc5f3b56a6fc488d62a12e12e1cf0d66ee
Anton Indrawan <anton.indrawan@gmail.com>
2020-04-03 18:22:58 +0200

    fix: build missing dependency

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ readonly source_dir=$(cd $(dirname ${BASH_SOURCE[0]});pwd)
 readonly build_dir=${source_dir}/output/build
 readonly package_dir=${source_dir}/output/package
 
-conan install ${source_dir} -if ${build_dir}
+conan install ${source_dir} -if ${build_dir} --build=missing
 
 # Build
     #Conan way for conanfile.py
